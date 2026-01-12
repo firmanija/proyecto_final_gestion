7e7240f768b3cc2c7a6219d1d619a4b071643105
Bruno Firmano <bruno.firmano@gmail.com>
2026-01-12 14:21:43 -0300

    adding more

--- data_managment.py
+++ data_managment.py
@@ -1,6 +1,5 @@
 import json
-from product import Product  # Asegúrate de importar la clase Product
-
+from product import Product  
 def save_products_to_json(products, filename='products.json'):
     """Saves the list of products to a JSON file."""
     with open(filename, 'w') as file:
